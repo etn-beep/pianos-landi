bfd0c058f972d2842771474e4953b87406e7ac4e
etn-beep <etiennelandi@proton.me>
2026-06-24 19:54:10 +0200

    Refactor RufflePlayer config for CDN usage
    Updated RufflePlayer configuration to use CDN for publicPath.

--- docs/javascripts/ruffle-config.js
+++ docs/javascripts/ruffle-config.js
@@ -1,16 +1,6 @@
-// On s'assure d'initialiser proprement l'objet sans l'écraser
 window.RufflePlayer = window.RufflePlayer || {};
-window.RufflePlayer.config = window.RufflePlayer.config || {};
-
-// On force le chemin absolu AVEC le sous-dossier javascripts
-window.RufflePlayer.config.publicPath = "https://tech-piano.com/javascripts/";
-window.RufflePlayer.config.allowScriptAccess = true;
-
-window.addEventListener("load", (event) => {
-    if (window.RufflePlayer && typeof window.RufflePlayer.newest === "function") {
-        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-        
-        // Optimisations mobiles sécurisées
-        window.RufflePlayer.config.quality = isMobile ? "low" : "high";
-    }
-});
+window.RufflePlayer.config = {
+    // Force Ruffle à charger ses moteurs d'exécution depuis le serveur CDN officiel
+    "publicPath": "https://unpkg.com",
+    "allowScriptAccess": true
+};
